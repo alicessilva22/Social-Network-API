329ce998ec0b387c5d8fdac65e84c2a7c9b0b2b9
alicessilva <alicessilva@aol.com>
2022-07-18 16:16:36 -0400

    user controller and route finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,48 @@
 const User = require('../models/User');
 
 module.exports = {
-  getUsers(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err));
-  },
-  getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with that ID' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new user
-  createUser(req, res) {
-    User.create(req.body)
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => res.status(500).json(err));
-  },
-  // update a user
-  updateSingleUser(req, res) {
-User.findOneAndUpdate({ })
-  },
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
+    // update a user
+    updateSingleUser(req, res) {
+        User.findOneAndUpdate({ name: '' }, { name: req.params.user }, { new: true }, (err, result) => {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                console.log('Uh Oh, something went wrong');
+                res.status(500).json({ message: 'something went wrong' });
+            }
+        });
+    },
+    // delete a user
+    deleteSingleUser(req, res) {
+        User.findOneAndDelete({ name: req.params.user }, (err, result) => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Deleted: ${result}`);
+            } else {
+                console.log('Uh Oh, something went wrong');
+                res.status(500).json({ message: 'something went wrong' });
+            }
+        });
+    }
 };
